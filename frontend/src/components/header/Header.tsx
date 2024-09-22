a06f8f3d1badfc0d25b562a439de790d84e69665
Pedro Seixas <pjfseixas.17@gmail.com>
2024-09-22 14:20:29 +0100

    Updated header and footer to be responsive. Updated theme for the store

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -3,21 +3,40 @@
 import Link from 'next/link'
 import UserStatus from './UserStatus'
 import { Menu } from 'lucide-react'
-import { useState } from 'react'
+import { Button } from "@/components/ui/button"
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
 
 export default function Header() {
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
-  
   return (
     <header className="bg-white shadow-md">
       <div className="container mx-auto px-4 py-4 flex items-center justify-between">
         <nav className="flex items-center space-x-4">
-          <button
-            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-            className="text-gray-600 hover:text-gray-800 md:hidden"
-          >
-            <Menu className="w-6 h-6" />
-          </button>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="secondary" size="icon" className="md:hidden">
+                <Menu className="w-6 h-6" />
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="left">
+              <SheetHeader>
+                <SheetTitle>Menu</SheetTitle>
+              </SheetHeader>
+              <nav className="flex flex-col space-y-4 mt-8">
+                <Link href="/" className="text-gray-600 hover:text-gray-800">
+                  Home
+                </Link>
+                <Link href="/products" className="text-gray-600 hover:text-gray-800">
+                  Products
+                </Link>
+              </nav>
+            </SheetContent>
+          </Sheet>
           <Link href="/" className="text-gray-600 hover:text-gray-800 hidden md:inline">
             Home
           </Link>
@@ -32,19 +51,6 @@ export default function Header() {
 
         <UserStatus/>
       </div>
-
-      {isMobileMenuOpen && (
-        <nav className="md:hidden border-t border-gray-200">
-          <div className="container mx-auto px-4 py-2 flex flex-col space-y-2">
-            <Link href="/" className="text-gray-600 hover:text-gray-800">
-              Home
-            </Link>
-            <Link href="/products" className="text-gray-600 hover:text-gray-800">
-              Products
-            </Link>
-          </div>
-        </nav>
-      )}
     </header>
   )
 }
